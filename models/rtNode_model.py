106c98709e9282c4ac283d12ed17431f6257fafb
Angel Dario <dariogomez1011@gmail.com>
2022-08-15 20:49:48 -0400

    created rtNode model

---
+++ models/rtNode_model.py
@@ -0,0 +1,11 @@
+from sqlalchemy.orm import declarative_base
+from sqlalchemy import Column, String, DateTime, Integer, Sequence
+from datetime import datetime
+
+Base = declarative_base()
+class rtNode(Base):
+    __tablename__='rtNodes'
+    id = Column(Integer(), Sequence('rtNode_id_sequence'), primary_key = True)
+    nodename = Column(String(25), nullable = False, unique = True)
+    city = Column(String(80), nullable = False, unique = False)
+    date_created = Column(DateTime(), default = datetime.now)
