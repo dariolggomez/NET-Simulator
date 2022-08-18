3b53f580df0c810db66f41d556de0516d9d2cc4e
Angel Dario <dariogomez1011@gmail.com>
2022-08-17 23:28:21 -0400

    updated rtNode and created netNode models

--- models/netNode_model.py
+++ models/netNode_model.py
@@ -3,8 +3,8 @@ from sqlalchemy import Column, String, DateTime, Integer, Sequence
 from datetime import datetime
 
 Base = declarative_base()
-class rtNode(Base):
-    __tablename__='rtNodes'
+class netNode(Base):
+    __tablename__ = 'netNodes'
     id = Column(Integer(), Sequence('rtNode_id_sequence'), primary_key = True)
     nodename = Column(String(25), nullable = False, unique = True)
     city = Column(String(80), nullable = False, unique = False)
