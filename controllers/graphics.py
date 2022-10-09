ddac3916bdec0bf1d000acb7e058431458023953
Angel Dario <dariogomez1011@gmail.com>
2022-10-09 14:36:38 -0400

    added spectrogram plot

--- controllers/graphics.py
+++ controllers/graphics.py
@@ -16,15 +16,16 @@ class GraphicsController():
     __mainWindow = None
     def __init__(self, parent) -> None:
         self.__mainWindow = parent
-        self.default_pen = pg.mkPen(width=0.9, color='y')
+        self.default_pen = pg.mkPen(width=1, color='y')
         self.default_pen.setStyle(Qt.PenStyle.SolidLine)
         self.createDefinitions()
         self.createWaveformPlot()
         self.createFftPlot()
+        self.create_spectrogram_graphic()
         # pg.setConfigOptions(useOpenGL=True)
         # pg.setConfigOption('enableExperimental', True)
 
-    def generatePgColormap(cm_name):
+    def generatePgColormap(self, cm_name):
         """Converts a matplotlib colormap to a pyqtgraph colormap."""
         colormap = plt.get_cmap(cm_name)
         colormap._init()
@@ -36,12 +37,12 @@ class GraphicsController():
         sfmt.setSwapInterval(0)
         QtGui.QSurfaceFormat.setDefaultFormat(sfmt)
         self.CHUNKSIZE = 1024
-        self.SAMPLE_RATE = 48000
+        self.SAMPLE_RATE = 44100
         self.TIME_VECTOR = np.arange(self.CHUNKSIZE) / self.SAMPLE_RATE
         self.N_FFT = 1024
         self.FREQ_VECTOR = np.fft.rfftfreq(self.N_FFT, d=self.TIME_VECTOR[1] - self.TIME_VECTOR[0])
         self.WATERFALL_FRAMES = int(250 * 2048 // self.N_FFT)
-        self.TIMEOUT = 81
+        self.TIMEOUT = 121
         self.fps = None
         self.EPS = 1e-8
         self.ptr = 0
@@ -66,7 +67,7 @@ class GraphicsController():
         self.waveform_plot.setYRange(-2 ** 15, 2 ** 15 - 1)
         self.waveform_plot.setLabel('left', "Señal", units='A.U.')
         self.waveform_plot.setLabel('bottom', "Tiempo", units='s')
-        self.curve = self.waveform_plot.plot(pen=self.default_pen, skipFiniteCheck=True, width=100)
+        self.curve = self.waveform_plot.plot(pen=self.default_pen, skipFiniteCheck=True)
         self.__mainWindow.ui.waveform_layout.addWidget(self.waveform_plot)
 
 
@@ -80,15 +81,16 @@ class GraphicsController():
             t_end = perf_counter()
             if(self.frames[0][0] != 0):
                 self.ptr += self.TIMEOUT
-                if(t_end - self.last_time >= 0.5):
-                    self.last_time = perf_counter()
-                    self.curve.setPos(self.ptr, 0)
+            # if(t_end - self.last_time >= 0.5):
+            # self.last_time = perf_counter()
+            self.curve.setPos(self.ptr, 0)
     
     @Slot()
     def startAll(self):
         self.startMicrophone()
         self.startWaveform()
         self.start_fft_plot()
+        self.start_spectrogram()
         self.running = True
     
     @Slot()
@@ -96,6 +98,7 @@ class GraphicsController():
         if self.running:
             self.stopWaveform()
             self.stop_fft_plot()
+            self.stop_spectrogram()
             self.recorder.stop = True
             self.running = False
 
@@ -109,7 +112,8 @@ class GraphicsController():
 
     def createFftPlot(self):
         self.fft_plot = pg.PlotWidget(title='Transformada de Fourier')
-        self.fft_curve = self.fft_plot.plot(pen=self.default_pen, skipFiniteCheck=True)
+        self.fft_curve = pg.PlotCurveItem(pen=self.default_pen, skipFiniteCheck=True)
+        self.fft_plot.addItem(self.fft_curve)
         self.fft_plot.enableAutoRange('y', True)
         self.fft_plot.setMouseEnabled(x=False, y=True)
         self.fft_plot.showGrid(x=True, y=True)
@@ -139,48 +143,51 @@ class GraphicsController():
         self.timer_fft.stop()
 
     # win.nextRow()
-
-    # image_data = np.random.rand(20, 20)
-    # waterfall_plot = win.addPlot(title='Waterfall plot', colspan=2)
-    # waterfall_plot.setLabel('left', "Frequency", units='Hz')
-    # waterfall_plot.showAxis('bottom', False)
-    # # waterfall_plot.setLabel('bottom', "Time", units='s')
-    # waterfall_plot.setXRange(0, WATERFALL_FRAMES * TIME_VECTOR.max())
-    # # waterfall_plot.enableAutoRange('x', True)
-    # waterfall_image = pg.ImageItem()
-    # waterfall_plot.addItem(waterfall_image)
-    # waterfall_image.setImage(image_data)
-    # lut = generatePgColormap('jet')
-    # waterfall_image.setLookupTable(lut)
-    # tr = QtGui.QTransform()
-    # tr.scale((CHUNKSIZE / SAMPLE_RATE), FREQ_VECTOR.max() * 2. / N_FFT)
-    # # set scale: x in seconds, y in Hz
-    # waterfall_image.setTransform(tr)
-
-
-    # def update_waterfall():
-    #     global waterfall_data, waterfall_image, last_time, fps, waterfall_plot
-    #     arr = np.c_[waterfall_data]
-    #     if arr.size > 0:
-    #         if arr.ndim == 1:
-    #             arr = arr[:, np.newaxis]
-    #         max = arr.max()
-    #         min = max / 10
-    #         waterfall_image.setImage(arr, levels=(min, max))
-    #         now = perf_counter()
-    #         dt = now - last_time
-    #         last_time = now
-    #         if fps is None:
-    #             fps = 1.0/dt
-    #         else:
-    #             s = np.clip(dt*3., 0, 1)
-    #             fps = fps * (1-s) + (1.0/dt) * s
-    #         waterfall_plot.setTitle('%0.2f fps' % fps)
+    def create_spectrogram_graphic(self):
+        # image_data = np.random.rand(20, 20)
+        self.waterfall_plot = pg.PlotWidget(title='Waterfall plot', colspan=2)
+        self.waterfall_plot.setLabel('left', "Frecuencia", units='Hz')
+        self.waterfall_plot.showAxis('bottom', False)
+        # waterfall_plot.setLabel('bottom', "Time", units='s')
+        self.waterfall_plot.setXRange(0, self.WATERFALL_FRAMES * self.TIME_VECTOR.max())
+        # waterfall_plot.enableAutoRange('x', True)
+        self.waterfall_image = pg.ImageItem()
+        self.waterfall_plot.addItem(self.waterfall_image)
+        # waterfall_image.setImage(image_data)
+        lut = self.generatePgColormap('jet')
+        self.waterfall_image.setLookupTable(lut)
+        tr = QtGui.QTransform()
+        tr.scale((self.CHUNKSIZE / self.SAMPLE_RATE), self.FREQ_VECTOR.max() * 2. / self.N_FFT)
+        # set scale: x in seconds, y in Hz
+        self.waterfall_image.setTransform(tr)
+        self.__mainWindow.ui.spectrogram_layout.addWidget(self.waterfall_plot)
 
 
-    # timer_waterfall = QtCore.QTimer()
-    # timer_waterfall.timeout.connect(update_waterfall)
-    # timer_waterfall.start(TIMEOUT)
+    def update_waterfall(self):
+        arr = np.c_[self.waterfall_data]
+        if arr.size > 0:
+            if arr.ndim == 1:
+                arr = arr[:, np.newaxis]
+            max = arr.max()
+            min = max / 10
+            self.waterfall_image.setImage(arr, levels=(min, max), autoLevels=False)
+            now = perf_counter()
+            dt = now - self.last_time
+            self.last_time = now
+            if self.fps is None:
+                self.fps = 1.0/dt
+            else:
+                s = np.clip(dt*3., 0, 1)
+                self.fps = self.fps * (1-s) + (1.0/dt) * s
+            self.waterfall_plot.setTitle('%0.2f fps' % self.fps)
+
+    def start_spectrogram(self):
+        self.timer_waterfall = QtCore.QTimer()
+        self.timer_waterfall.timeout.connect(self.update_waterfall)
+        self.timer_waterfall.start(self.TIMEOUT)
+    
+    def stop_spectrogram(self):
+        self.timer_waterfall.stop()
 
     # if __name__ == '__main__':
     #     import sys
